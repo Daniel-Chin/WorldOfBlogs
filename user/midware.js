3ce2c47195378e7150bbafbf5e5427a84cbbca58
Daniel Chin <daniel_chin@yahoo.com>
2019-11-17 10:52:01 -0500

    move userAuth midware to user/midware

---
+++ user/midware.js
@@ -0,0 +1,26 @@
+const safeCompare = require('safe-compare');
+const { USERS, getDb } = require('../db');
+const { COOKIE } = require('./helper');
+
+const authMidware = async (req, res, next) => {
+  req.whois = null;
+  const { username, token } = req.cookies;
+  if (username) {
+    const user = await getDb(USERS, username);
+    if (user && user.token) {
+      if (user.token.deadline > Date.now()) {
+        if (safeCompare(token, user.token.value)) {
+          req.whois = username;
+          next();
+          return;
+        }
+      }
+    }
+  }
+  // clear cookies
+  res.cookie(COOKIE.USERNAME, 0, { maxAge: Date.now() });
+  res.cookie(COOKIE.TOKEN, 0, { maxAge: Date.now() });
+  res.status(403).send('403 Forbidden: User auth failed');
+};
+
+module.exports = authMidware;
