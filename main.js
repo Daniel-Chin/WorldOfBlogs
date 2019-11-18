3e3235ff5078c496aad71da4752c422704c19edb
Daniel Chin <daniel_chin@yahoo.com>
2019-11-18 15:59:32 -0500

    express relays frontend

--- main.js
+++ main.js
@@ -5,8 +5,20 @@ const apiRouter = require('./apiRoute');
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+if (process.env.PROD) {
+  console.log('Production Environment!');
+} else {
+  console.log('Dev Environment! Using CORS!');
+  const cors = require('cors');
+  app.use(cors());
+}
 
 app.use('/api', apiRouter);
+app.use('/ui', express.static('./front/build/'));
+
+app.get('/', (_, res) => {
+  res.redirect('/ui');
+});
 
 app.listen(PORT, function () {
   console.log(`Listening @ port ${PORT}`);
