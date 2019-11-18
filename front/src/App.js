87e9fd9718047c57b198740fbd34f4e375a5e907
Daniel Chin <daniel_chin@yahoo.com>
2019-11-18 17:01:08 -0500

    aboutThis page

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import AboutThis from './container/AboutThis';
+import RegisterPage from './container/RegisterPage';
+import LoginPage from './container/LoginPage';
+import LogoutPage from './container/LogoutPage';
+import ViewPage from './container/ViewPage';
+import DashboardPage from './container/DashboardPage';
+import UserPage from './container/UserPage';
+import HistoryPage from './container/HistoryPage';
+import MinePage from './container/MinePage';
+import EditPage from './container/EditPage';
+import NewPage from './container/NewPage';
+import InvalidCookiePage from './container/InvalidCookiePage';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Switch>
+      <Route exact path='/' render={AboutThis} />
+      <Route exact path='/register' render={RegisterPage} />
+      <Route exact path='/login' render={LoginPage} />
+      <Route exact path='/logout' render={LogoutPage} />
+      <Route exact path='/view' render={ViewPage} />
+      <Route exact path='/dashboard' render={DashboardPage} />
+      <Route exact path='/user/:username' render={UserPage} />
+      <Route exact path='/history/:index' render={HistoryPage} />
+      <Route exact path='/mine/:index' render={MinePage} />
+      <Route exact path='/edit/:mine_index' render={EditPage} />
+      <Route exact path='/new' render={NewPage} />
+      <Route exact path='/invalid-cookie' render={InvalidCookiePage} />
+      <Route>
+        404 No such URL
+      </Route>
+    </Switch>
   );
-}
+};
 
 export default App;
---
+++ front/src/container/AboutThis.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const AboutThis = () => {
+  return (
+    <div className='centerAlign' style={{
+      marginBottom: '30px',
+    }}>
+      <h1>World of Blogs</h1>
+      <p>
+        Facebook, YouTube, Twitter... <br />
+        They always show us <br />
+        what we like to see. 
+      </p>
+      <p>
+        It is easy to live in a bubble <br />
+        where all we see and hear <br />
+        feels right to us. 
+      </p>
+      <p>
+        Little by little, <br />
+        we start to forget <br />
+        that there exists another side <br />
+        of the story. <br />
+        We start to forget <br />
+        how to empathize <br />
+        with people of different minds. 
+      </p>
+      <p>
+        It is time we stop. 
+      </p>
+      <p>
+        In <b>World of Blogs</b>, <br />
+        you can't choose what to read. <br />
+        We show you <br />
+        completely random blogs  <br/>
+        written by <br />
+        completely random authors. <br/>
+        You shall read the unbiased world, <br />
+        one blog at a time. 
+      </p>
+      <div className='button greenButton' tabIndex={0}>
+        Start
+      </div>
+    </div>
+  );
+};
+
+export default AboutThis;
