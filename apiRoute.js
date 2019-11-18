aabc20079b370c7aebaa9cfebad10afc98e231dc
Daniel Chin <daniel_chin@yahoo.com>
2019-11-18 15:10:02 -0500

    API all methods drafted, not tested

--- apiRoute.js
+++ apiRoute.js
@@ -3,6 +3,13 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const { authMidware, userRouter } = require('./user/index');
+const { 
+  USERS, BLOGS, DELETED, getDb, setDb, opinionTransact, 
+  newBlogTransact, purgeBlog, 
+} = require('./db');
+const { MAX_READ_TIME, estimateReadTime } = require('./misc');
+
+const REJECTION_MAX_TRY = 32;
 
 const apiRouter = express.Router();
 apiRouter.use(cookieParser());    // req.cookies
@@ -10,5 +17,179 @@ apiRouter.use(bodyParser.urlencoded({ extended: false }));
 apiRouter.use(bodyParser.json());
 
 apiRouter.use('/user', userRouter);
+
+apiRouter.get('/view', authMidware, async (req, res) => {
+  const history = req.user.history;
+  const last_view = history[history.length - 1];
+  if (last_view) {
+    if (last_view.expire > Date.now()) {
+      // Last view not expired yet
+      res.json(false);
+      return;
+    }
+  }
+  const max_id = (await getDb('max_id', 'value')).value;
+  let blog;
+  let tries = 0;
+  while (! blog) {
+    if (tries ++ >= REJECTION_MAX_TRY) {
+      res.json(false);
+      return;
+    }
+    blog = await getDb(BLOGS, 
+      Math.floor(Math.random() * (max_id + 1)).toString()
+    );
+  }
+  res.json(blogAddOpinionDelId(blog, req.user));
+
+  // update user.history
+  await setDb(USERS, req.user.name, {
+    ...req.user, 
+    history: [
+      ...history, 
+      {
+        blog: blog.id, 
+        expire: Date.now() + blog.read_time, 
+      }, 
+    ], 
+  });
+});
+
+apiRouter.get('/rate', authMidware, (req, res) => {
+  const opinion = req.query.opinion;
+  if (! ['like', 'hate', 'none'].includes(opinion)) {
+    res.end();  // Being rude to hackers is ok
+    return;
+  }
+  const history = req.user.history;
+  const blog_id_to_rate = history[history.length - 1].blog; // may throw TypeError, ok
+  if (req.user.opinions[blog_id_to_rate]) {
+    res.status(405).send(
+      '405 METHOD NOT ALLOWED: Rating a blog more than once'
+    );
+    return;
+  }
+  const promise_0 = setDb(USERS, req.user.name, {
+    ...req.user, 
+    opinions: {
+      ...req.user.opinions, 
+      [blog_id_to_rate]: opinion, 
+    }, 
+  });
+  const promise_1 = opinionTransact(blog_id_to_rate, opinion);
+  Promise.all([promise_0, promise_1]).then(() => {
+    res.send('ok');
+  });
+});
+
+apiRouter.get('/listMine', authMidware, (req, res) => {
+  Promise.all(req.user.mine.map(
+    (blog_id) => {
+      if (blog_id === DELETED) {
+        return DELETED;
+      }
+      return getDb(BLOGS, blog_id);
+    }
+  )).then((blogs) => {
+    res.json(blogs.map(
+      (blog) => {
+        if (blog === DELETED) {
+          return false;
+        }
+        const { title, last_modified } = blog;
+        return { title, last_modified };
+      }
+    ));
+  });
+});
+
+apiRouter.get('/listHistory', authMidware, (req, res) => {
+  Promise.all(req.user.history.map(
+    ({ blog }) => {
+      if (blog === DELETED) {
+        return DELETED; 
+      }
+      return getDb(BLOGS, blog);
+    }
+  )).then((blogs) => {
+    res.json(blogs.map(
+      (blog) => {
+        if (blog === DELETED) {
+          return false;
+        }
+        const { title, owner, last_modified } = blog;
+        return { title, owner, last_modified };
+      }
+    ));
+  });
+});
+
+apiRouter.get('/getMine', authMidware, async (req, res) => {
+  const blog_id = req.user.mine[req.query.mine_index];
+  const blog = await getDb(BLOGS, blog_id);
+  res.json(blogAddOpinionDelId(blog, req.user));
+});
+
+apiRouter.get('/getHistory', authMidware, async (req, res) => {
+  const blog_id = req.user.history[req.query.history_index].blog;
+  const blog = await getDb(BLOGS, blog_id);
+  res.json(blogAddOpinionDelId(blog, req.user));
+});
+
+apiRouter.get('/addBlog', authMidware, async (req, res) => {
+  const blog_id = await newBlogTransact(req.user);
+  const mine = req.user.mine;
+  await setDb(USERS, req.user.name, {
+    ...req.user, 
+    mine: [...mine, blog_id], 
+  });
+  res.json(mine.length);
+});
+
+apiRouter.post('/editBlog', authMidware, async (req, res) => {
+  const { title, content, mine_index } = req.body;
+  if (estimateReadTime({ title, content }) > MAX_READ_TIME) {
+    res.json({
+      is_ok: false, 
+      message: 'Blog too long. ', 
+    });
+    return;
+  }
+  const blog_id = req.user.mine[mine_index];
+  const blog = await getDb(BLOGS, blog_id);
+  if (! blog) {
+    res.json({
+      is_ok: false, 
+      message: 'Blog does not exist. ', 
+    });
+    return;
+  }
+  await setDb(BLOGS, blog_id, {
+    ...blog, 
+    title, 
+    content, 
+  });
+  res.json({
+    is_ok: true, 
+  });
+});
+
+apiRouter.get('/delBlog', authMidware, async (req, res) => {
+  const blog_id = req.user.mine[req.query.mine_index];
+  if (blog_id === DELETED) {
+    res.end();
+    return;
+  }
+  await purgeBlog(blog_id);
+  res.send('ok');
+});
+
+const blogAddOpinionDelId = (blog, user) => (
+  {
+    ...blog, 
+    id: null, 
+    my_opinion: user.opinions[blog.id], 
+  }
+);
 
 module.exports = apiRouter;
--- db.js
+++ db.js
@@ -4,6 +4,8 @@ const admin = require('firebase-admin');
 
 const USERS = 'users';
 const BLOGS = 'blogs';
+const HOLES = 'holes';
+const DELETED = 'deleted';
 
 let serviceAccount;
 if (process.env.SERVICE) {
@@ -15,7 +17,7 @@ if (process.env.SERVICE) {
 }
 
 console.log('initializeApp...');
-const fb = admin.initializeApp({
+admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://dyn-web-final-8p3.firebaseio.com", 
 });
@@ -54,7 +56,98 @@ const registerTransact = async (new_user) => {
     t.set(userRef, new_user);
     return Promise.resolve(true);
   });
-}
+};
+
+const opinionTransact = async (blog_id, opinion) => {
+  const blogRef = db.collection(BLOGS).doc(blog_id);
+  return await db.runTransaction(async (t) => {
+    const blog = (await t.get(blogRef)).data();
+    if (opinion === 'like') {
+      blog.likes ++;
+    } else if (opinion === 'hate') {
+      blog.hates ++;
+    }
+    t.set(blogRef, blog);
+    return Promise.resolve(true);
+  });
+};
+
+const newBlogTransact = async (user) => {
+  const maxIdRef = db.collection('max_id').doc('value');
+  const holes = db.collection(HOLES);
+  return await db.runTransaction(async (t) => {
+    const to_wait = []; // promises to wait for at the end
+    let new_id;
+    const first_hole = (await t.get(holes.doc('head'))).data().next;
+    if (first_hole !== -1) {
+      // fill hole
+      new_id = first_hole;
+      const second_hole = (await t.get(
+        holes.doc(first_hole.toString())
+      )).data().next;
+      to_wait.push(t.delete(
+        holes.doc(first_hole.toString())
+      ));
+      to_wait.push(t.set(holes.doc('head'), { next: second_hole }));
+    } else {
+      // append
+      new_id = (await t.get(maxIdRef)).data().value + 1;
+      to_wait.push(t.set(maxIdRef, { value: new_id }));
+    }
+    new_id = new_id.toString();
+    t.set(db.collection(BLOGS).doc(new_id), {
+      id: new_id, 
+      title: 'dafault', 
+      content: 'default', 
+      likes: 0, hates: 0, 
+      owner: user.name, 
+      last_modified: Date.now(), 
+      read_time: 0, 
+    });
+    await Promise.all(to_wait);
+    return Promise.resolve(new_id);
+  });
+};
+
+const purgeBlog = async (blog_id) => {
+  const usersRef = db.collection(USERS);
+  const holesRef = db.collection(HOLES);
+  return await db.runTransaction(async (t) => {
+    const [hole, snapshot] = await Promise.all([
+      t.get(holesRef.doc('head')), 
+      t.get(usersRef), 
+    ]);
+    const first_hole = hole.data().next;
+    t.set(holesRef.doc('head'), { next: blog_id });
+    t.set(holesRef.doc(blog_id), { next: first_hole });
+    snapshot.forEach((doc) => {
+      const user = doc.data();
+      let modified = false;
+      user.mine = user.mine.map((x) => {
+        if (x === blog_id) {
+          modified = true;
+          return DELETED;
+        }
+        return x;
+      });
+      user.history = user.history.map((x) => {
+        if (x.blog === blog_id) {
+          modified = true;
+          return {
+            ...x, 
+            blog: DELETED, 
+          };
+        }
+        return x;
+      });
+      if (modified) {
+        t.set(USERS.doc(doc.id), user);
+      }
+    });
+    t.delete(db.collection(BLOGS).doc(blog_id));
+    return Promise.resolve(true);
+  });
+};
 
 module.exports = {
   USERS, BLOGS, 
@@ -62,4 +155,7 @@ module.exports = {
   setDb, 
   getAllBlogs,
   registerTransact,
+  opinionTransact, 
+  newBlogTransact, 
+  purgeBlog, 
 };
