577b6f35a5b04a5fe45602c2f9da25cfb93b6083
Daniel Chin <daniel_chin@yahoo.com>
2019-11-18 15:36:50 -0500

    Blog parser

---
+++ front/src/helper/blogUtils.js
@@ -0,0 +1,32 @@
+const MAX_READ_TIME = 0;
+
+const wordTime = (word) => (
+  .17 + .1 * word.length
+);
+
+const parseText = (text) => {
+  return text.split(' ').map((word) => ({
+    word, 
+    time: wordTime(word), 
+  }));
+};
+
+const parseBlog = ({ title, content }) => {
+  return {
+    title: parseText(title), 
+    content: parseText(content), 
+  }
+};
+
+const estimateReadTime = (blog) => {
+  const parsed = blog.parsed || parseBlog(blog);
+  return parsed.title.reduce((acc, { time }) => (acc + time))
+    + 1
+    + parsed.content.reduce((acc, { time }) => (acc + time));
+};
+
+module.exports = {
+  MAX_READ_TIME, 
+  parseBlog, 
+  estimateReadTime, 
+};
