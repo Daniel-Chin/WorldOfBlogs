c78c8210fd3858237a38762d7d4cbbc7bb6991bf
Daniel Chin <daniel_chin@yahoo.com>
2019-11-19 18:40:48 -0500

    mobile view starts with 100%

--- front/src/index.js
+++ front/src/index.js
@@ -7,6 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <HashRouter>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <App />
   </HashRouter>, 
   document.getElementById('root')
