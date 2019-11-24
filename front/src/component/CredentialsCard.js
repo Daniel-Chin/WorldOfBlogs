8c2097da56abdd29c685a47eead8beae12f9fb0d
Daniel Chin <daniel_chin@yahoo.com>
2019-11-23 21:13:32 -0500

    More flexible credentialCard; Mobile credentailCard has no margin

--- front/src/component/CredentialsCard.js
+++ front/src/component/CredentialsCard.js
@@ -2,23 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { InputGroup, FormControl } from 'react-bootstrap';
 import CheckUsernameCard from './CheckUsernameCard';
-import { enterMeansClick } from '../helper/misc';
+import { enterMeansClick, is_mobile } from '../helper/misc';
 
 const CredentialsCard = ({ 
   title, fetch_state, onEnter, available, otherwise, 
   onUsernameChange, onPasswordChange, onUsernameBlur,
-  button_class, 
+  button_class, button_configs, 
 }) => {
+  const effective_button_configs = button_configs || [
+    { name: 'Username', type: 'text' }, 
+    { name: 'Password', type: 'password' }, 
+  ];
+  const buttons = effective_button_configs.map((x) => (x.name));
+  const button_type = effective_button_configs.map((x) => (x.type));
   return (
-    <div className='credentialsCard centerAlign'>
+    <div className={'centerAlign credentialsCard' + (
+      is_mobile ? 'Mobile' : ''
+    )}>
       <h2>{title}</h2>
       <InputGroup className='mt-3'>
         <InputGroup.Prepend>
-          <InputGroup.Text id="username_field">Username</InputGroup.Text>
+          <InputGroup.Text id={buttons[0]}>{buttons[0]}</InputGroup.Text>
         </InputGroup.Prepend>
         <FormControl
-          aria-label="Username"
-          aria-describedby="username_field"
+          type={button_type[0]}
+          aria-label={buttons[0]}
+          aria-describedby={buttons[0]}
           onBlur={onUsernameBlur}
           onChange={onUsernameChange}
           onKeyUp={enterMeansClick(onEnter, false)}
@@ -29,12 +38,12 @@ const CredentialsCard = ({
       }
       <InputGroup className='mt-3'>
         <InputGroup.Prepend>
-          <InputGroup.Text id="password_field">Password</InputGroup.Text>
+          <InputGroup.Text id={buttons[1]}>{buttons[1]}</InputGroup.Text>
         </InputGroup.Prepend>
         <FormControl
-          type='password'
-          aria-label="Password"
-          aria-describedby="password_field"
+          type={button_type[1]}
+          aria-label={buttons[1]}
+          aria-describedby={buttons[1]}
           onChange={onPasswordChange}
           onKeyUp={enterMeansClick(onEnter, false)}
         />
@@ -51,12 +60,14 @@ const CredentialsCard = ({
           Loading...
         </p>
       }
-      <p className='mb-0'>
-        Otherwise,{' '}
-        <Link to={otherwise.to}>
-          {otherwise.text}
-        </Link>
-      </p>
+      {otherwise &&
+        <p className='mb-0'>
+          Otherwise,{' '}
+          <Link to={otherwise.to}>
+            {otherwise.text}
+          </Link>
+        </p>
+      }
     </div>
   );
 };
