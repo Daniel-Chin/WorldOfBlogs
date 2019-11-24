1e2ec0dbb799209db454ab7c531e77dba622e3f0
Daniel Chin <daniel_chin@yahoo.com>
2019-11-23 19:09:16 -0500

    fix typewritter

--- front/src/component/TypeWriter.js
+++ front/src/component/TypeWriter.js
@@ -1,37 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
-const TypeWriter = ({ parsed, onEnd, access_time }) => {
+const INTRO_TIME = 4500;
+
+const TypeWriter = ({ parsed, onEnd, access_time, title_time }) => {
   const [n_paragraph, setN_paragraph] = useState(1);
   const [n_line, setN_line] = useState(1);
   const [n_word, setN_word] = useState(1);
   const [finished, setFinished] = useState(false);
-  const [fast_forward, setFast_forward] = useState(
-    function () {
-      const time_delta = Date.now() - access_time;
-      if (time_delta < 7000) {
-        return 0
-      }
-      return time_delta;
-    }()
-  );
+  const [time_passed, setTime_passed] = useState(0);
 
   useEffect(() => {
     if (finished || parsed === null) return;
-    let { time } = parsed[n_paragraph - 1][n_line - 1][n_word - 1];
-    if (fast_forward > 0) {
+    const word_time = parsed[n_paragraph - 1][n_line - 1][n_word - 1].time;
+    let time = Date.now() - access_time - time_passed - title_time - INTRO_TIME;
+    if (time > 0) {
+      console.log(time);
       time = 1;
+    } else {
+      time = word_time;
     }
     const timeout = setTimeout(() => {
-      setFast_forward(fast_forward - time + 1);
+      setTime_passed((x) => (x + word_time));
+      console.log('p', time_passed);
       if (n_word === parsed[n_paragraph - 1][n_line - 1].length) {
         if (n_line === parsed[n_paragraph - 1].length) {
           if (n_paragraph === parsed.length) {
             setFinished(true);
             onEnd();
           } else {
-            setN_paragraph(n_paragraph + 1);
-            setN_line(1);
             setN_word(1);
+            setN_line(1);
+            setN_paragraph(n_paragraph + 1);
           }
         } else {
           setN_word(1);
@@ -46,7 +45,8 @@ const TypeWriter = ({ parsed, onEnd, access_time }) => {
     n_paragraph, n_line, n_word, 
     setN_paragraph, setN_line, setN_word, 
     onEnd, parsed, finished, setFinished,
-    fast_forward, setFast_forward, 
+    time_passed, setTime_passed, 
+    access_time, title_time, 
   ]);
 
   if (parsed === null) {
@@ -66,14 +66,16 @@ const TypeWriter = ({ parsed, onEnd, access_time }) => {
               paragraph.slice(0, n_line)
             :
               paragraph
-            ).map((line, i) => (
-              <span key={i}>
-                {(i === n_line - 1 ?
+            ).map((line, j) => (
+              <span key={j}>
+                {((
+                  (i === n_paragraph - 1) && (j === n_line - 1)
+                ) ?
                   line.slice(0, n_word)
                 :
                   line
-                ).map((word, i) => (
-                  <span key={i}>
+                ).map((word, k) => (
+                  <span key={k}>
                     {word.word}{' '}
                   </span>
                 ))}
